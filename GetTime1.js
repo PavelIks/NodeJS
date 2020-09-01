62d8fa1e52971c89d5f631cc4b1fd271aff71c2d
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-09-01 15:05:16 +0300

    Updated-1.

--- GetTime1.js
+++ GetTime1.js
@@ -6,5 +6,6 @@
 	else if	(hour >= 12 && hour < 18)	return "Добрый день!";
 	else if	(hour >= 18 && hour < 24)	return "Добрый вечер!";
 	else if	(hour >= 24 && hour < 5)	return "Доброй ночи!";
+	
 }
 GetTime()
--- GetTime2.js
+++ GetTime2.js
@@ -8,5 +8,6 @@
 	else if	(hour >= 18 && hour < 24)	{ greeting = "Добрый вечер!"; }
 	else if	(hour >= 24 && hour < 5)	{ greeting = "Доброй ночи!"; }
 	return greeting;
+	
 }
 GetTime()
