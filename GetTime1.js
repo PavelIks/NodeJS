7728d819ec9561bc0ae4217ea045d6fc49c13edc
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-09-01 14:40:30 +0300

    first commit

---
+++ GetTime1.js
@@ -0,0 +1,10 @@
+﻿function GetTime()
+{
+	var hour = new Date().getHours();
+
+	if	(hour >= 5 && hour < 12)	return "Доброе утро!"; 
+	else if	(hour >= 12 && hour < 18)	return "Добрый день!";
+	else if	(hour >= 18 && hour < 24)	return "Добрый вечер!";
+	else if	(hour >= 24 && hour < 5)	return "Доброй ночи!";
+}
+GetTime()
---
+++ GetTime2.js
@@ -0,0 +1,12 @@
+﻿function GetTime()
+{
+	var date = new Date();
+	var hour = date.getHours();
+
+	if (hour >= 5 && hour < 12) { greeting = "Доброе утро!"; }
+	else if (hour >= 12 && hour < 18) { greeting = "Добрый день!"; }
+	else if (hour >= 18 && hour < 24) { greeting = "Добрый вечер!"; }
+	else if (hour >= 24 && hour < 5) { greeting = "Доброй ночи!"; }
+	return greeting;
+}
+GetTime()
